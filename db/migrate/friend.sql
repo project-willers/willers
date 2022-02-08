daaaeadd4212e8e25fd934f80a218aa95e0a12ad
lufeee <contact@lufe.jp>
2022-02-08 23:44:41 +0900

    modelのリファクタリング

--- db/migrate/friend.sql
+++ db/migrate/friend.sql
@@ -1,11 +1,7 @@
 -- +migrate Up
 CREATE DATABASE IF NOT EXISTS willers;
 CREATE TABLE IF NOT EXISTS willers.friends (
-  id  BIGINT  NOT NULL  AUTO_INCREMENT,
   name  VARCHAR(255)	NOT NULL,
   other VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE (name),
-  UNIQUE (other)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- +migrate Down
