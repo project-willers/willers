387217e970bf93043b556b5b6ac2c2dce4f8025d
lufeee <contact@lufe.jp>
2022-02-11 16:12:41 +0900

    Add tables

--- db/migrate/account.sql
+++ db/migrate/account.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS willers;
 CREATE TABLE IF NOT EXISTS willers.accounts (
   name		VARCHAR(50)	NOT NULL,
   email VARCHAR(50) NOT NULL,
-  password	VARCHAR(255) 	NOT NULL,
+  password	VARCHAR(513) 	NOT NULL,
   PRIMARY KEY (name),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- +migrate Down
