6809cca67a08f7afe62556839c064b22ed1dc70e
lufeee <contact@lufe.jp>
2022-02-12 03:34:16 +0900

    日記機能の追加 認証なし

--- api/handler/diary.go
+++ api/handler/diary.go
@@ -1,19 +1,78 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"willers-api/model"
+
+	"github.com/labstack/echo/v4"
+)
 
 func DiaryWrite(c echo.Context) error {
-	return nil
+	diary := new(model.Diary)
+	if err := c.Bind(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	if err := validate.Struct(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	// debug
+	fmt.Fprintln(os.Stdout, diary)
+	if err := model.AddDiary(diary); err != nil {
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusOK, "")
 }
 
 func DiaryRead(c echo.Context) error {
-	return nil
+	// TODO
+	// jwt tokenからusernameを取得
+	userName := ""
+	// debug
+	fmt.Fprintln(os.Stdout, userName)
+
+	diaries, err := model.GetDiary(userName)
+	if err != nil {
+		return err
+	}
+
+	json, err := json.Marshal(diaries)
+	return c.JSON(http.StatusOK, json)
 }
 
 func DiaryEdit(c echo.Context) error {
-	return nil
+	diary := new(model.Diary)
+	if err := c.Bind(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	if err := validate.Struct(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	// debug
+	fmt.Fprintln(os.Stdout, diary)
+	if err := model.UpdateDiary(diary); err != nil {
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusOK, "")
 }
 
 func DiaryDelete(c echo.Context) error {
-	return nil
+	diary := new(model.Diary)
+	if err := c.Bind(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	if err := validate.Struct(diary); err != nil {
+		return echo.ErrBadRequest
+	}
+	// debug
+	fmt.Fprintln(os.Stdout, diary)
+	if err := model.DeleteDiary(diary); err != nil {
+		return echo.ErrInternalServerError
+	}
+
+	return c.JSON(http.StatusOK, "")
 }
--- api/model/diary.go
+++ api/model/diary.go
@@ -1,31 +1,69 @@
 package model
 
-import "time"
+import (
+	"context"
+	"time"
+	"willers-api/db"
+)
 
 type Diary struct {
-	Name      string    `json:"name"`
+	UserName  string    `json:"name"`
 	Content   string    `json:"content"`
-	SelectAt  time.Time `json:"time"`
+	SelectAt  time.Time `json:"select"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type Diaries []*Diary
 
-func GetDiary() (Diaries, error) {
+func GetDiary(name string) (Diaries, error) {
+	result, err := db.Database.QueryContext(context.Background(), "SELECT * FROM diaries WHERE name=?", name)
+	if err != nil {
+		return nil, err
+	}
+
+	var diaries Diaries
+	for result.Next() {
+		d := &Diary{}
+		if err := result.Scan(d.UserName, d.Content, d.SelectAt, d.CreatedAt, d.UpdatedAt); err != nil {
+			return nil, err
+		}
+		diaries = append(diaries, d)
+	}
+	return diaries, nil
+}
+
+func UpdateDiary(diary *Diary) error {
+	if _, err := GetDiary(diary.UserName); err != nil {
+		return err
+	}
+
+	result := db.Database.QueryRowContext(context.Background(), "UPDATE diaries SET content=? WHERE name=? AND select_at=?", diary.Content, diary.UserName, diary.SelectAt)
+	if err := result.Scan(diary.UserName, diary.Content, diary.SelectAt, diary.CreatedAt, diary.UpdatedAt); err != nil {
+		return err
+	}
 	return nil
 }
 
-func UpdateDiary() error {
-	return nil
-}
-
-func AddDiary() error {
+func AddDiary(diary *Diary) error {
 	// TODO
 	// すでに存在しているならUpdateDiaryにRedirectさせる処理
+	// フロント処理にした方がいい？
+	if _, err := GetDiary(diary.UserName); err != nil {
+		return err
+	}
+
+	result := db.Database.QueryRowContext(context.Background(), "INSERT INTO diaries(name, content, select_at) VALUE(?, ?, ?)", diary.UserName, diary.Content, diary.SelectAt)
+	if err := result.Scan(diary.UserName, diary.Content, diary.SelectAt, diary.CreatedAt, diary.UpdatedAt); err != nil {
+		return err
+	}
 	return nil
 }
 
-func DeleteDiary() error {
+func DeleteDiary(diary *Diary) error {
+	result := db.Database.QueryRowContext(context.Background(), "DELETE FROM diaries WHERE name=? AND content=?, select_at", diary.UserName, diary.Content, diary.SelectAt)
+	if err := result.Scan(diary.UserName, diary.Content, diary.SelectAt); err != nil {
+		return err
+	}
 	return nil
 }
